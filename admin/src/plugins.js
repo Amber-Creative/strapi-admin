f56ffe2ff542485857097642b059ed6b4660e310
jason <jason_clb@outlook.com>
2023-03-06 13:51:06 +0800

    updated gitignore and commented plugin imports.

---
+++ admin/src/plugins.js
@@ -0,0 +1,14 @@
+
+// To override this file create a plugins-dev.js one and copy the content of the plugin.js one.
+// When starting the app the script will copy the plugins-dev.js into this one instead.
+// import contentTypeBuilder from '../../../content-type-builder/admin/src';
+// import email from '../../../email/admin/src';
+// import upload from '../../../upload/admin/src';
+
+const plugins = {
+  // 'content-type-builder': contentTypeBuilder,
+  // 'email': email,
+  // 'upload': upload,
+};
+  
+export default plugins;
